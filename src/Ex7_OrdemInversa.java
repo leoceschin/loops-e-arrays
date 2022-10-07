109fb4d11ca9e3a596ebc06157a7322a1eceec24
Leona <leonardofcoceschin@gmail.com>
2022-10-07 19:17:57 -0300

    resolvido o exercício Ordem Inversa

---
+++ src/Ex7_OrdemInversa.java
@@ -0,0 +1,10 @@
+public class Ex7_OrdemInversa {
+    public static void main(String[] args) {
+        int[] vetor = {1,2,3,4,5,6};
+
+        for(int i = (vetor.length - 1); i >= 0; i--){
+            System.out.println(vetor[i]);
+        }
+    }
+    
+}
